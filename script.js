4757c931096ae67adce9495ab9644046faec1817
yor-jimenez <yorleyuribe@gmail.com>
2022-10-31 21:20:40 -0300

    input

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const task = document.querySelector("[data-task]");
+    const value = input.value;
+    // backticks (comillas invertidas)
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    console.log(content);
 }
 
 console.log(btn);
