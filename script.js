aed2f487313b879d9a774a5cae98fb553c258347
yor-jimenez <yorleyuribe@gmail.com>
2022-11-01 21:25:54 -0300

    clase 03 terminada

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    const task = document.querySelector("[data-task]");
     const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+
     // backticks (comillas invertidas)
     const content = `<div>
     <i class="far fa-check-square icon"></i>
     <span class="task">${value}</span>
     </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
     task.innerHTML = content;
+
+    list.appendChild(task);
+
     console.log(content);
 }
 
